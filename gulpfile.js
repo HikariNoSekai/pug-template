4ce4244ab2ac7b954925620e47861683b33cf3b6
Igor Stasyuk <obvilion08@gmail.com>
2018-08-17 13:34:28 +0300

    added async into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -14,11 +14,12 @@ const gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     pug = require('gulp-pug'),
     plumber = require('gulp-plumber'),
-	sourcemaps = require('gulp-sourcemaps'),
-	mixins = require('postcss-mixins');
+    sourcemaps = require('gulp-sourcemaps'),
+    mixins = require('postcss-mixins');
+const watch = require('gulp-watch');
 
 
-gulp.task('css', function() {
+gulp.task('css', function () {
 
     let processors = [
         precss,
@@ -26,22 +27,20 @@ gulp.task('css', function() {
         cssnext,
         short,
         csso,
-		mixins
+        mixins
     ];
 
     return gulp.src('src/assets/main.css')
-		.pipe(sourcemaps.init())
-		.pipe(plumber())
-		.pipe(postcss(processors))
-		.pipe(rename({
-			extname: '.css',
-			suffix: '.min'
-		}))
-		.pipe(sourcemaps.write('./'))
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(postcss(processors))
+        .pipe(rename({
+            extname: '.css',
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./dist/style/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+        .pipe(browserSync.stream())
 });
 
 gulp.task('html', function buildHTML() {
@@ -51,36 +50,37 @@ gulp.task('html', function buildHTML() {
             pretty: '\t'
         }))
         .pipe(gulp.dest('dist/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+        .pipe(browserSync.stream())
 });
 
 
 
-gulp.task('js', function() {
+gulp.task('js', function () {
     return gulp.src('src/js/main.js')
-		.pipe(sourcemaps.init())
-		.pipe(plumber())
-		.pipe(browserify({
-			debug: true,
-			transform: [babelify.configure({
-				presets: ['env']
-			})]
-		}))
-		.pipe(uglify())
-		.pipe(rename({
-			extname: '.js',
-			suffix: '.min'
-		}))
-		.pipe(sourcemaps.write('./'))
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(browserify({
+            debug: true,
+            transform: [babelify.configure({
+                presets: ['env']
+            })]
+        }))
+        .pipe(uglify())
+        .pipe(rename({
+            extname: '.js',
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./dist/js'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+        .pipe(browserSync.stream())
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('fonts', function () {
+    return gulp.src('src/fonts/*.*')
+        .pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('reload', function () {
 
     browserSync({
         server: {
@@ -90,9 +90,9 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('watch', ['browser-sync'], function() {
-    gulp.watch('src/**/*.css', [('css')]);
-    gulp.watch('src/**/*.pug', [('html')]);
-    gulp.watch('src/**/*.js', [('js')]);
-    gulp.watch('dist/*.html', browserSync.reload);
+gulp.task('watch', ['reload', 'js', 'css', 'html', 'fonts'], function () {
+    watch('src/**/*.pug', () => gulp.start('html'));
+    watch('src/**/*.css', () => gulp.start('css'));
+    watch('src/**/*.js', () => gulp.start('js'));
+    gulp.watch('dist/*.html', browserSync.reload());
 });
